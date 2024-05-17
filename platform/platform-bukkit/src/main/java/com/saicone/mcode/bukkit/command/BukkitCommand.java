4cea75ee15edf598bdac2c2b50eaa9203669fbdb
Rubenicos <44579213+Rubenicos@users.noreply.github.com>
2024-05-17 10:45:59 -0600

    Add bukkit command dispatch

--- platform/platform-bukkit/src/main/java/com/saicone/mcode/bukkit/command/BukkitCommand.java
+++ platform/platform-bukkit/src/main/java/com/saicone/mcode/bukkit/command/BukkitCommand.java
@@ -2,11 +2,10 @@ package com.saicone.mcode.bukkit.command;
 
 import com.saicone.mcode.module.command.*;
 import com.saicone.mcode.module.command.CommandBuilder;
+import com.saicone.mcode.module.command.CommandExecutor;
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandMap;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.PluginCommand;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
@@ -86,6 +85,49 @@ public class BukkitCommand {
         commands.remove(command.getName(), command);
         for (String alias : command.getAliases()) {
             commands.remove(alias, command);
+        }
+    }
+
+    @NotNull
+    public static CommandResult dispatch(@NotNull CommandSender user, @NotNull String command) throws CommandException {
+        final int index = command.indexOf(' ');
+        if (index > 0 && index + 1 < command.length()) {
+            return dispatch(user, command.substring(0, index), command.substring(index + 1));
+        } else {
+            return dispatch(user, command.trim(), "");
+        }
+    }
+
+    @NotNull
+    public static CommandResult dispatch(@NotNull CommandSender user, @NotNull String id, @NotNull String input) throws CommandException {
+        return dispatch(user, id, input, false);
+    }
+
+    @NotNull
+    @SuppressWarnings("unchecked")
+    public static CommandResult dispatch(@NotNull CommandSender user, @NotNull String id, @NotNull String input, boolean silent) throws CommandException {
+        final Command command = all().get(id.toLowerCase());
+        if (command == null) {
+            if (!silent) {
+                if (user instanceof Player) {
+                    user.sendMessage("Unknown command. Type \"/help\" for help.");
+                } else {
+                    user.sendMessage("Unknown command. Type \"help\" for help.");
+                }
+            }
+            return CommandResult.NOT_FOUND;
+        }
+
+        try {
+            if (command instanceof CommandExecutor) {
+                return ((CommandExecutor<CommandSender>) command).result(user, id, input.split(" "));
+            }
+            final boolean result = command.execute(user, id, input.split(" "));
+            return result ? CommandResult.DONE : CommandResult.FAIL_SYNTAX;
+        } catch (CommandException ex) {
+            throw ex;
+        } catch (Throwable ex) {
+            throw new CommandException("Unhandled exception executing '" + id + " " + input + "' in " + command, ex);
         }
     }
 
