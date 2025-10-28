db723354dae4dda7132306295024739c8ebe7e97
Rubén <44579213+Rubenicos@users.noreply.github.com>
2025-10-28 07:59:23 -0600

    Test built-in dependencies before load

--- bootstrap/src/main/java/com/saicone/mcode/bootstrap/Addon.java
+++ bootstrap/src/main/java/com/saicone/mcode/bootstrap/Addon.java
@@ -10,32 +10,66 @@ import java.util.Map;
 public enum Addon {
 
     // Common
-    COMMON("${mcode_group}:common:${mcode_version}"),
+    COMMON("${mcode_group}:common:${mcode_version}",
+            "com.saicone.mcode.Platform"
+    ),
 
     // Modules
-    MODULE_COMMAND("${mcode_group}:module-command:${mcode_version}"),
-    MODULE_COMMAND_BRIGADIER("${mcode_group}:module-command-brigadier:${mcode_version}"),
-    MODULE_ENV("${mcode_group}:module-env:${mcode_version}"),
-    MODULE_LANG("${mcode_group}:module-lang:${mcode_version}"),
-    MODULE_LANG_ADVENTURE("${mcode_group}:module-lang-adventure:${mcode_version}"),
-    MODULE_SCRIPT("${mcode_group}:module-script:${mcode_version}"),
+    MODULE_COMMAND("${mcode_group}:module-command:${mcode_version}",
+            "com.saicone.mcode.module.command.CommandNode"
+    ),
+    MODULE_COMMAND_BRIGADIER("${mcode_group}:module-command-brigadier:${mcode_version}",
+            "com.saicone.mcode.module.command.brigadier.BrigadierCommandBuilder"
+    ),
+    MODULE_ENV("${mcode_group}:module-env:${mcode_version}",
+            "com.saicone.mcode.env.Env"
+    ),
+    MODULE_LANG("${mcode_group}:module-lang:${mcode_version}",
+            "com.saicone.mcode.module.lang.AbstractLang"
+    ),
+    MODULE_LANG_ADVENTURE("${mcode_group}:module-lang-adventure:${mcode_version}",
+            "com.saicone.mcode.module.lang.AdventureLang"
+    ),
+    MODULE_SCRIPT("${mcode_group}:module-script:${mcode_version}",
+            "com.saicone.mcode.module.script.Script"
+    ),
     MODULE_SCRIPT_JS("${mcode_group}:module-script-js:${mcode_version}"),
-    MODULE_TASK("${mcode_group}:module-task:${mcode_version}"),
+    MODULE_TASK("${mcode_group}:module-task:${mcode_version}",
+            "com.saicone.mcode.module.task.Scheduler"
+    ),
 
     // Extensions
-    EXTENSION_CACHE("${mcode_group}:extension-cache:${mcode_version}"),
-    EXTENSION_LOOKUP("${mcode_group}:extension-lookup:${mcode_version}"),
+    EXTENSION_CACHE("${mcode_group}:extension-cache:${mcode_version}",
+            "com.saicone.mcode.util.cache.Cache"
+    ),
+    EXTENSION_LOOKUP("${mcode_group}:extension-lookup:${mcode_version}",
+            "com.saicone.mcode.util.invoke.EasyLookup"
+    ),
 
     // Platforms
-    PLATFORM_BUKKIT("${mcode_group}:platform-bukkit:${mcode_version}"),
-    PLATFORM_BUNGEE("${mcode_group}:platform-bungee:${mcode_version}"),
-    PLATFORM_PAPER("${mcode_group}:platform-paper:${mcode_version}"),
-    PLATFORM_VELOCITY("${mcode_group}:platform-velocity:${mcode_version}"),
+    PLATFORM_BUKKIT("${mcode_group}:platform-bukkit:${mcode_version}",
+            "com.saicone.mcode.bukkit.BukkitPlatform"
+    ),
+    PLATFORM_BUNGEE("${mcode_group}:platform-bungee:${mcode_version}",
+            "com.saicone.mcode.bungee.BungeePlatform"
+    ),
+    PLATFORM_PAPER("${mcode_group}:platform-paper:${mcode_version}",
+            "com.saicone.mcode.paper.scheduler.PaperScheduler"
+    ),
+    PLATFORM_VELOCITY("${mcode_group}:platform-velocity:${mcode_version}",
+            "com.saicone.mcode.velocity.VelocityPlatform"
+    ),
 
     // Libraries
-    LIBRARY_DELIVERY4J("com{}saicone{}delivery4j:delivery4j:${delivery4j_version}"),
-    LIBRARY_SETTINGS("com{}saicone{}settings:settings:${settings_version}"),
-    LIBRARY_TYPES("com{}saicone:types:${types_version}");
+    LIBRARY_DELIVERY4J("com{}saicone{}delivery4j:delivery4j:${delivery4j_version}",
+            "com.saicone.delivery4j.Broker"
+    ),
+    LIBRARY_SETTINGS("com{}saicone{}settings:settings:${settings_version}",
+            "com.saicone.settings.Settings"
+    ),
+    LIBRARY_TYPES("com{}saicone:types:${types_version}",
+            "com.saicone.types.Types"
+    );
 
     public static final Addon[] VALUES = values();
     public static final Map<String, String> RELOCATIONS;
@@ -67,8 +101,11 @@ public enum Addon {
 
     private final EzlibLoader.Dependency dependency;
 
-    Addon(@NotNull String path) {
-        this.dependency = new EzlibLoader.Dependency().path(path.replace("{}", ".")).repository("https://jitpack.io/");
+    Addon(@NotNull String path, @NotNull String... test) {
+        this.dependency = new EzlibLoader.Dependency()
+                .path(path.replace("{}", "."))
+                .repository("https://jitpack.io/")
+                .test(test);
     }
 
     public boolean isModule() {
